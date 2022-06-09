569500eab45cd2430fab93fef117dfe8d1eda2bd
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-06-10 02:28:18 +0900

    Submit abc248-c,d

--- cargo-compete/abc248/src/bin/c.rs
+++ cargo-compete/abc248/src/bin/c.rs
@@ -3,9 +3,26 @@ use std::fmt::Write;
 
 #[allow(non_snake_case)]
 fn main() {
-    input!{
-        
+    input! {
+        N: u64,
+        M: u64,
+        K: u64,
     }
 
-    todo!();
+    //(N*1) * (K+1)
+    let modulo = 998244353;
+    let mut dp = vec![vec![0; (K + 1) as usize]; (N + 1) as usize];
+    dp[0][0] = 1;
+    for i in 0..N {
+        for j in 0..=K {
+            for a in 1..=M {
+                if j + a <= K {
+                    dp[i as usize + 1][(j + a) as usize] += dp[i as usize][j as usize];
+                    dp[i as usize + 1][(j + a) as usize] %= modulo;
+                }
+            }
+        }
+    }
+    let ans = dp[N as usize].iter().sum::<u64>() % modulo;
+    println!("{}", ans);
 }
--- cargo-compete/abc248/src/bin/d.rs
+++ cargo-compete/abc248/src/bin/d.rs
@@ -1,11 +1,34 @@
 use proconio::input;
+use proconio::marker::Usize1;
+use std::collections::BTreeMap;
 use std::fmt::Write;
+use superslice::Ext;
 
 #[allow(non_snake_case)]
 fn main() {
-    input!{
-        
+    input! {
+        N: usize,
+        A: [u32;N],
+        Q: usize,
+        q: [(Usize1, Usize1, u32);Q],
     }
 
-    todo!();
+    let mut m = BTreeMap::<u32, Vec<usize>>::new();
+    for (i, &a) in A.iter().enumerate() {
+        if let Some(e) = m.get_mut(&a) {
+            e.push(i);
+        } else {
+            m.insert(a, vec![i]);
+        }
+    }
+    let mut ans = String::new();
+    for (L, R, X) in q {
+        if let Some(e) = m.get(&X) {
+            let c = e.lower_bound(&(R + 1)) - e.lower_bound(&L);
+            writeln!(ans, "{}", c);
+        } else {
+            writeln!(ans, "{}", 0);
+        }
+    }
+    print!("{}", ans);
 }
