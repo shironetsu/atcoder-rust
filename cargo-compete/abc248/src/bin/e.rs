1d727e1eaf0080df34811150b9035799b2b4ca1b
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-06-10 22:14:06 +0900

    Refactor abc248-e

--- cargo-compete/abc248/src/bin/e.rs
+++ cargo-compete/abc248/src/bin/e.rs
@@ -7,7 +7,7 @@ use std::collections::BTreeMap;
 fn main() {
     input! {
         N: usize,
-        K: usize,
+        K: u64,
         P: [(i64, i64);N],
     }
 
@@ -23,40 +23,26 @@ fn main() {
         let mut a = y0 - y1;
         let mut b = -x0 + x1;
         let mut c = x0 * y1 - x1 * y0;
-        if a < 0 {
-            a *= -1;
-            b *= -1;
-            c *= -1;
-        }
-        if a == 0 {
+        let p = if a == 0 {
             if b < 0 {
                 b *= -1;
                 c *= -1;
             }
             let g = num_integer::gcd(b, c.abs());
-            let p = (0, b / g, c / g);
-            if let Some(e) = conj.get_mut(&p) {
-                *e += 1;
-            } else {
-                conj.insert(p, 1);
-            }
+            (0, b / g, c / g)
         } else {
-            let g = num_integer::gcd(a, b.abs());
-            let p = (a / g, b / g, c / g);
-            if let Some(e) = conj.get_mut(&p) {
-                *e += 1;
-            } else {
-                conj.insert(p, 1);
+            if a < 0 {
+                a *= -1;
+                b *= -1;
+                c *= -1;
             }
-        }
+            let g = num_integer::gcd(a, b.abs());
+            (a / g, b / g, c / g)
+        };
+        *conj.entry(p).or_insert(1) += 1;
     }
 
-    let mut ans = 0;
-    for (_, &c) in conj.iter() {
-        let n = (1 + num_integer::sqrt(1 + 8 * c)) / 2;
-        if n >= K as u64 {
-            ans += 1;
-        }
-    }
+    let ans = conj.iter().filter(|(_, &c)| c >= K * (K - 1) / 2).count();
+
     println!("{}", ans);
 }
