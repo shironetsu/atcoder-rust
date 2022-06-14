f58738114b2d499899c0edd932dd75998da6ff33
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-06-15 01:47:46 +0900

    Add note on abc245-e

--- cargo-compete/abc245/src/bin/e.rs
+++ cargo-compete/abc245/src/bin/e.rs
@@ -1,10 +1,8 @@
 use proconio::input;
-#[allow(unused_imports)]
-use proconio::marker::{Bytes, Chars, Isize1, Usize1};
 use std::collections::BTreeMap;
-#[allow(unused_imports)]
-use std::fmt::Write;
-//use std::ops::Bound::{Excluded, Included, Unbounded};
+
+const CHOCO: i32 = 0;
+const BOX: i32 = 1;
 
 #[allow(non_snake_case)]
 fn main() {
@@ -17,28 +15,25 @@ fn main() {
         D: [i64;M],
     }
 
+    //box first
     let mut arr = vec![];
     for i in 0..N {
-        arr.push((A[i], B[i], 0));
+        arr.push((A[i], B[i], CHOCO));
     }
     for i in 0..M {
-        arr.push((C[i], D[i], 1));
+        arr.push((C[i], D[i], BOX));
     }
-    //box first
-    arr.sort_by_key(|(x, y, t)| (-x, -y, -t));
-
-    let mut widths = BTreeMap::<i64, u64>::new();
-    for &(x, y, t) in arr.iter() {
-        //println!("{} {} {}", x, y, t);
-        if t == 1 {
-            *widths.entry(y).or_default() += 1;
+    arr.sort_by_key(|&(x, y, t)| (-x, -y, -t));
+    let mut ys = BTreeMap::<i64, u64>::new();
+    for &(_, y, t) in arr.iter() {
+        if t == BOX {
+            *ys.entry(y).or_default() += 1;
         } else {
-            if let Some((&ymin, &c)) = widths.range(y..).next(){
-                //println!("- {} {}", ymin, c);
+            if let Some((&ymin, &c)) = ys.range(y..).next() {
                 if c == 1 {
-                    widths.remove(&ymin);
+                    ys.remove(&ymin);
                 } else {
-                    *widths.get_mut(&ymin).unwrap() -= 1;
+                    *ys.get_mut(&ymin).unwrap() -= 1;
                 }
             } else {
                 println!("No");
