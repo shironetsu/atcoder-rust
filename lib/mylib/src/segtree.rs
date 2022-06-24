5860ba575c8b949b7f40d1bab8c06d0a247aab06
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-06-24 23:30:22 +0900

    Implement segtree

--- lib/mylib/src/segtree.rs
+++ lib/mylib/src/segtree.rs
@@ -1,53 +1,58 @@
-pub trait Monoid {
-    fn op(&self, rhs: &Self) -> Self;
-    fn e() -> Self;
-}
+use cargo_snippet::snippet;
 
-impl Monoid for i32 {
-    fn op(&self, rhs: &Self) -> Self {
-        *self.min(rhs)
+#[snippet("segtree-sum")]
+#[snippet(include="sum-impl")]
+#[snippet(include="segtree")]
+pub struct Sum {}
+
+#[snippet("sum-impl")]
+impl Op<i64> for Sum {
+    fn op(lhs: &i64, rhs: &i64) -> i64 {
+        lhs + rhs
     }
 
-    fn e() -> Self {
-        i32::MAX
+    fn e() -> i64 {
+        0
     }
 }
 
-#[derive(Debug)]
-pub struct SegTree<T: Monoid> {
+#[snippet("segtree-op")]
+pub trait Op<S> {
+    fn op(lhs: &S, rhs: &S) -> S;
+    fn e() -> S;
+}
+
+#[snippet("segtree")]
+#[snippet(include = "segtree-impl")]
+#[snippet(include = "segtree-op")]
+pub struct SegmentTree<S: Clone, F: Op<S>> {
+    n: usize,
     log: usize,
     size: usize,
-    d: Vec<T>,
+    op: F,
+    d: Vec<S>,
 }
 
-fn ceil_pow2(n: usize) -> usize {
-    let mut m = 1;
-    let mut log = 0;
-    while n > m {
-        m <<= 1;
-        log += 1;
-    }
-    log
-}
-
-impl<S: Monoid> SegTree<S> {
-    pub fn from(v: Vec<S>) -> SegTree<S> {
+#[snippet("segtree-impl")]
+impl<S: Clone, F: Op<S>> SegmentTree<S, F> {
+    pub fn from(op: F, v: Vec<S>) -> SegmentTree<S, F> {
         let n = v.len();
-        let log = ceil_pow2(n);
+        let log = Self::ceil_pow2(n);
         let size = 1 << log;
-        let mut d = vec![];
-        for _ in 0..2 * size {
-            d.push(S::e());
-        }
+        let mut d = (0..2 * size).map(|_| F::e()).collect::<Vec<_>>();
         for (i, x) in v.into_iter().enumerate() {
-            d[size + i] = x;
+            d[size + i] = x
         }
-
-        let mut st = SegTree { d, log, size };
-        for i in (1..=size - 1).rev() {
+        let mut st = SegmentTree {
+            n,
+            log,
+            size,
+            op,
+            d,
+        };
+        for i in (1..=(size - 1)).rev() {
             st.update(i);
         }
-
         st
     }
 
@@ -59,71 +64,46 @@ impl<S: Monoid> SegTree<S> {
         }
     }
 
-    pub fn get(&self, p: usize) -> Option<&S> {
-        self.d.get(p + self.size)
+    pub fn get(&self, p: usize) -> &S {
+        &self.d[p + self.size]
     }
 
-    pub fn prod(&self, l: usize, r: usize) -> S {
-        let mut sml = S::e();
-        let mut smr = S::e();
+    pub fn prod(&mut self, l: usize, r: usize) -> S {
+        let mut sml = F::e();
+        let mut smr = F::e();
         let mut l = l + self.size;
         let mut r = r + self.size;
 
         while l < r {
             if l & 1 == 1 {
-                sml = sml.op(&self.d[l]);
+                sml = F::op(&sml, &self.d[l]);
                 l += 1;
             }
             if r & 1 == 1 {
                 r -= 1;
-                smr = self.d[r].op(&smr);
+                smr = F::op(&self.d[r], &smr);
+                l >>= 1;
+                r >>= 1;
             }
-
-            l >>= 1;
-            r >>= 1;
         }
-        sml.op(&smr)
+        F::op(&sml, &smr)
     }
 
-    pub fn all_prod(&self) -> &S {
-        self.d.get(1).unwrap()
+    pub fn all_prod(&self) -> S {
+        self.d[1].clone()
     }
 
     fn update(&mut self, k: usize) {
-        self.d[k] = self.d[2 * k].op(&self.d[2 * k + 1]);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn ceil_pow2_works() {
-        assert_eq!(ceil_pow2(1), 0);
-        assert_eq!(ceil_pow2(2), 1);
-        assert_eq!(ceil_pow2(3), 2);
-        assert_eq!(ceil_pow2(4), 2);
-        assert_eq!(ceil_pow2(15), 4);
-        assert_eq!(ceil_pow2(16), 4);
-        assert_eq!(ceil_pow2(17), 5);
+        self.d[k] = F::op(&self.d[2 * k], &self.d[2 * k + 1]);
     }
 
-    #[test]
-    fn all_prod_works() {
-        let mut st = SegTree::from(vec![3, 1, 4, 1, 5, 9]);
-        assert_eq!(*st.all_prod(), 1);
-        st.set(1, 6);
-        st.set(3, 3);
-        assert_eq!(*st.all_prod(), 3);
-    }
-
-    
-    #[test]
-    fn prod_works() {
-        let mut st = SegTree::from(vec![3, 1, 4, 1, 5, 9]);
-        assert_eq!(st.prod(0, 1), 3);
-        assert_eq!(st.prod(0, 2), 1);
-        assert_eq!(st.prod(4, 6), 5);
+    fn ceil_pow2(n: usize) -> usize {
+        let mut m = 1;
+        let mut log = 0;
+        while n > m {
+            m <<= 1;
+            log += 1;
+        }
+        log
     }
 }
