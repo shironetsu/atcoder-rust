cc35ec164339fe6f9526a7aa2c9fb9389fc8ef0a
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-07-16 18:04:52 +0900

    Submit abc259-f

--- cargo-compete/abc259/src/bin/f.rs
+++ cargo-compete/abc259/src/bin/f.rs
@@ -8,42 +8,68 @@ use std::collections::*;
 use std::fmt::Write;
 use superslice::{Ext, Ext2};
 
+const INF: i64 = 2_000_000_000;
+
 #[fastout]
 fn main() {
     input! {
         N: usize,
-        d: [usize;N],
+        d: [i64;N],
+        uvw: [(Usize1, Usize1, i64);N-1],
     }
+
+    let mut w = BTreeMap::<(usize, usize), i64>::new();
     let mut ad = vec![vec![]; N];
-    let mut w = vec![0; N-1];
-    let mut p = vec![];
-    for i in 0..N - 1 {
-        input! {
-            u: Usize1,
-            v: Usize1,
-            ww: i64,
-        }
+    for &(u, v, ww) in uvw.iter() {
         ad[u].push(v);
         ad[v].push(u);
-        w[i] = ww;
-        p.push((ww, (u, v)));
+        w.insert((u, v), ww);
+        w.insert((v, u), ww);
     }
 
-    p.sort();
+    let mut seen = vec![false; N];
+    seen[0] = true;
+    let (lte, lt) = dfs(0, &mut seen, &ad, &w, &d);
+    println!("{}", lte);
+}
 
-    let mut d = d;
-    let mut ans = 0;
-    for i in (0..N - 1).rev() {
-        let (ww, (u, v)) = p[i];
-        if ww <= 0 {
+fn dfs(
+    u: usize,
+    seen: &mut Vec<bool>,
+    ad: &Vec<Vec<usize>>,
+    w: &BTreeMap<(usize, usize), i64>,
+    d: &Vec<i64>,
+) -> (i64, i64) {
+    let mut bh = BinaryHeap::<(i64, i64, i64)>::new();
+    for &v in ad[u].iter() {
+        if seen[v] {
+            continue;
+        }
+        seen[v] = true;
+        let (lte, lt) = dfs(v, seen, ad, w, d);
+        let delta = if d[v] > 0 { w.get(&(u, v)).unwrap() - (lte - lt) } else { -INF };
+        bh.push((delta, lt, lte));
+    }
+
+    let mut count = 0;
+    let mut lte = 0;
+    let mut lt = 0;
+    loop {
+        if let Some((delta, lt0, lte0)) = bh.pop() {
+            count += 1;
+            if count < d[u] && delta > 0 {
+                lte += delta + lte0;
+                lt += delta + lte0;
+            } else if count == d[u] && delta > 0 {
+                lte += delta + lte0;
+                lt += lte0;
+            } else {
+                lte += lte0;
+                lt += lte0;
+            }
+        } else {
             break;
         }
-        if d[u].min(d[v]) >= 1 {
-            d[u] -= 1;
-            d[v] -= 1;
-            ans += ww;
-        }
     }
-
-    println!("{}", ans);
+    (lte, lt)
 }
