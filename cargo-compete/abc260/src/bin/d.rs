7923ca03d1bf52f71eedeed17960d47415bb3231
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-07-18 01:08:40 +0900

    Submit abc260-d,e

--- cargo-compete/abc260/src/bin/d.rs
+++ cargo-compete/abc260/src/bin/d.rs
@@ -6,7 +6,6 @@ use proconio::marker::{Bytes, Chars, Isize1, Usize1};
 use proconio::{fastout, input};
 use std::collections::*;
 use std::fmt::Write;
-use std::ops::Bound::{Included, Unbounded};
 use superslice::{Ext, Ext2};
 
 #[fastout]
@@ -14,78 +13,43 @@ fn main() {
     input! {
         N: usize,
         K: usize,
-        P:[Usize1;N],
+        P: [Usize1;N],
     }
 
-    if K ==1 {
-        let mut ans = vec![0;N];
-        for (i, &p) in P.iter().enumerate(){
-            ans[p] = i;
-        }
-        for a in ans {
-            println!("{}", a + 1);
-        }
-        return;
-    }
-
-    let mut ans = vec![-1; N];
-    let mut yama = BTreeMap::<usize, usize>::new();
-    let mut vv: Vec<Vec<usize>> = vec![];
+    let mut under = vec![None; N];
+    let mut pile = vec![0; N];
+    let mut ans = vec![None; N];
+    let mut top = BTreeSet::<usize>::new();
     for (i, &p) in P.iter().enumerate() {
-        let mut kcopy = 0usize;
-        let mut some = true;
-        let mut rem = false;
-        if let Some((&k, &j)) = yama.range((Included(&p), Unbounded)).next() {
-            //let mut v = v;
-            kcopy = k;
-            yama.insert(p, j);
-            vv[j].push(p);
-            if vv[j].len() == K {
-                rem = true;
-                for &u in vv[j].iter() {
-                    ans[u] = (i + 1) as i32;
+        if let Some(&t) = top.range(p..).next() {
+            under[p] = Some(t);
+            pile[p] = (pile[t] + 1) as i32;
+            top.remove(&t);
+        } else {
+            pile[p] = 1;
+        }
+        
+        top.insert(p);
+
+        if pile[p] == K as i32 {
+            let mut j = p;
+            loop {
+                ans[j] = Some(i);
+                if let Some(k) = under[j] {
+                    j = k;
+                } else {
+                    break;
                 }
             }
-        } else {
-            some = false;
-            vv.push(vec![p]);
-            yama.insert(p, vv.len() - 1);
-        }
-        if some {
-            yama.remove(&kcopy);
-        }
-        if rem {
-            yama.remove(&p);
+            top.remove(&p);
         }
     }
+    let ans = ans
+        .iter()
+        .map(|op| op.map_or(-1, |i| i as i32 + 1))
+        .collect_vec();
 
     for a in ans {
         println!("{}", a);
     }
-
-    // let mut top = vec![];
-    // let mut yama = vec![];
-    // let mut ans = vec![-1;N];
-    // let mut h = 0;
-    // for i in 0..N{
-    //     let j = top[h..].upper_bound(&P[i]);
-    //     let j = j + h;
-    //     //println!("{:?}", top.iter().copied().map(|i|{i+1}).collect_vec());
-    //     if j >= top.len() {
-    //         top.push(P[i]);
-    //         yama.push(vec![P[i]]);
-    //     } else {
-    //         top[j] = P[i];
-    //         yama[j].push(P[i]);
-    //     }
-    //     if yama[j].len() % K == 0 {
-    //         h += 1;
-    //         for &k in yama[j].iter().rev().take(K){
-    //             ans[k] = (i + 1) as i32;
-    //         }
-    //     }
-    // }
-    // for a in ans{
-    //     println!("{}", a);
-    // }
 }
