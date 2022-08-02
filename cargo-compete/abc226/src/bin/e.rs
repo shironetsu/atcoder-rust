301eb8e26d5829db5e385d57938b1d271ba38fdc
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-08-03 00:47:45 +0900

    Submit abc226-e

--- cargo-compete/abc226/src/bin/e.rs
+++ cargo-compete/abc226/src/bin/e.rs
@@ -7,12 +7,193 @@ use std::collections::*;
 use maplit::*;
 use itertools::*;
 use superslice::{Ext, Ext2};
+use petgraph::unionfind::UnionFind;
 
 #[fastout]
 fn main() {
     input!{
-        
+        N: usize,
+        M: usize,
+        UV: [(Usize1, Usize1);M],
     }
 
-    
+    let mut ut = UnionFind::new(N);
+
+    let mut res = vec![];
+    for &(u, v) in UV.iter(){
+        if ut.equiv(u, v) {
+            res.push((u, v));
+        } else {
+            ut.union(u, v);
+        }
+    }
+
+    let mut found_loop = vec![false;N];
+
+    for &(u, v) in res.iter(){
+        let r = ut.find(u);
+        let s = ut.find(v);
+        if found_loop[r] || found_loop[s] {
+            println!("0");
+            return;
+        }
+        ut.union(u, v);
+        let r = ut.find(u);
+        found_loop[r] = true;
+    }
+
+    let mut reps = ut.into_labeling().into_iter().collect::<BTreeSet<_>>();
+    for &r in reps.iter() {
+        if !found_loop[r] {
+            println!("0");
+            return;
+        }
+    }
+
+    let ans = ModInt::from(2).pow(reps.len() as i64);
+    println!("{}", ans.val);
+}
+
+use petgraph::stable_graph::IndexType;
+pub trait UnionFindExt {
+    fn into_sizes(self) -> Vec<usize>;
+}
+impl<K: IndexType> UnionFindExt for UnionFind<K> {
+    fn into_sizes(self) -> Vec<usize> {
+        let reps = self.into_labeling();
+        let mut sizes = BTreeMap::<K, usize>::new();
+        for r in reps {
+            *sizes.entry(r).or_default() += 1;
+        }
+        sizes.into_iter().map(|(_, size)| size).collect::<Vec<_>>()
+    }
+}
+
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
+impl Sub<i64> for ModInt {
+    type Output = Self;
+    fn sub(self, rhs: i64) -> Self {
+        let val = (self.val - rhs).rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+}
+impl SubAssign for ModInt {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.val = (self.val - rhs.val).rem_euclid(Self::MODULO);
+    }
+}
+impl Sub for ModInt {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        let val = (self.val - rhs.val).rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+}
+impl Mul<i64> for ModInt {
+    type Output = Self;
+    fn mul(self, rhs: i64) -> Self {
+        let val = (self.val * rhs).rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+}
+impl MulAssign for ModInt {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.val = (self.val * rhs.val).rem_euclid(Self::MODULO);
+    }
+}
+impl Mul for ModInt {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        let val = (self.val * rhs.val).rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+}
+impl Inv for ModInt {
+    fn inv(self) -> Self {
+        let mut x = 0;
+        let mut y = 0;
+        gcd(self.val, Self::MODULO, &mut x, &mut y);
+        ModInt {
+            val: x.rem_euclid(Self::MODULO),
+        }
+    }
+}
+pub trait Inv {
+    fn inv(self) -> Self;
+}
+pub fn gcd(a: i64, b: i64, x: &mut i64, y: &mut i64) -> i64 {
+    if a != 0 {
+        let mut d = gcd(b % a, a, x, y);
+        *y -= (b / a) * *x;
+        std::mem::swap(x, y);
+        if d < 0 {
+            d = -d;
+            *x = -*x;
+            *y = -*y;
+        }
+        d
+    } else {
+        *x = 0;
+        *y = 1;
+        b
+    }
+}
+impl ModInt {
+    pub const MODULO: i64 = 998_244_353;
+    pub fn new() -> ModInt {
+        ModInt { val: 0 }
+    }
+    pub fn from(x: i64) -> ModInt {
+        let val = x.rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+    pub fn binom(n: i64, k: i64) -> ModInt {
+        let mut val = ModInt::from(1);
+        for i in 1..=k {
+            val *= ModInt::from(n - i + 1) * ModInt::from(i).inv()
+        }
+        val
+    }
+    pub fn pow(&self, n: i64) -> ModInt {
+        if n == 0 {
+            return ModInt::from(1);
+        }
+        let (a, n) = if n > 0 {
+            (self.clone(), n)
+        } else {
+            (self.inv(), -n)
+        };
+        let mut k = 0;
+        while n >= 1 << k {
+            k += 1;
+        }
+        let mut x = ModInt::from(1);
+        for i in (0..k).rev() {
+            x = if (n >> i) & 1 == 1 { x * x * a } else { x * x };
+        }
+        x
+    }
+}
+impl Add<i64> for ModInt {
+    type Output = Self;
+    fn add(self, rhs: i64) -> Self {
+        let val = (self.val + rhs).rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+}
+impl AddAssign for ModInt {
+    fn add_assign(&mut self, rhs: Self) {
+        self.val = (self.val + rhs.val).rem_euclid(Self::MODULO);
+    }
+}
+impl Add for ModInt {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        let val = (self.val + rhs.val).rem_euclid(Self::MODULO);
+        ModInt { val }
+    }
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct ModInt {
+    val: i64,
 }
