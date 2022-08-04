c0b83eed406b343c9cb216fd86657a8154d64a22
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-08-04 23:49:57 +0900

    Submit abc224-d

--- cargo-compete/abc224/src/bin/d.rs
+++ cargo-compete/abc224/src/bin/d.rs
@@ -16,7 +16,42 @@ fn main() {
         p: [Usize1;8],
     }
 
-    
+    let mut ad = vec![vec![];9];
+    for (u, v) in uv{
+        ad[u].push(v);
+        ad[v].push(u);
+    }
 
-    
+    let mut dist = BTreeMap::new();
+
+    let mut init = [None;9];
+    for (i, &x) in p.iter().enumerate(){
+        init[x] = Some(i);
+    }
+    //dist.insert(init, 0);
+    let mut todo = VecDeque::new();
+    todo.push_back((init, 0));
+    loop {
+        if let Some((s, d)) = todo.pop_front(){
+            if !dist.contains_key(&s){
+                dist.insert(s, d);
+                let (empty, _) = s.iter().enumerate().find(|&x|*x.1 == None).unwrap();
+                for &v in ad[empty].iter(){
+                    let mut t = s;
+                    t[empty] = s[v];
+                    t[v] = None;
+                    todo.push_back((t, d+1));
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    let dest = (0..9).map(|x| if x < 8 { Some(x) } else { None }).collect_vec();
+    if let Some(ans) = dist.get(&dest[..]){
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
 }
