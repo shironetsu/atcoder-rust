1fa4bd8ca6b75af49630d2ab7ed4fd9e55f47e46
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-08-11 22:21:05 +0900

    Submit abc220-d

--- cargo-compete/abc220/src/bin/d.rs
+++ cargo-compete/abc220/src/bin/d.rs
@@ -12,10 +12,26 @@ use superslice::{Ext, Ext2};
 fn main() {
     input!{
         N: usize,
-        A: [i32;N],
+        A: [usize;N],
     }
 
-    
+    let m = 998244353i64;
+
+    let mut dp = vec![vec![0i64;10];N];
+    dp[0][A[0]] = 1;
+    for i in 0..N-1{
+        for j in 0..10{
+            dp[i+1][(j+A[i+1])%10] += dp[i][j];
+            dp[i+1][(j+A[i+1])%10] %= m;
+            dp[i+1][(j*A[i+1])%10] += dp[i][j];
+            dp[i+1][(j*A[i+1])%10] %= m;
+        }
+    }
+    for i in 0..10{
+        println!("{}", dp[N-1][i]);
+    }
+
+
 
     
 }
