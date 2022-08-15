db024cc305583d81aed1e4c1c1e4fc8912a01f63
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-08-15 23:57:51 +0900

    Submit abc218-a

---
+++ cargo-compete/abc218/src/bin/a.rs
@@ -0,0 +1,25 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+use proconio::{input, fastout};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::fmt::Write;
+use std::collections::*;
+use maplit::*;
+use itertools::*;
+use superslice::{Ext, Ext2};
+
+#[fastout]
+fn main() {
+    input!{
+        N: Usize1,
+        S: Chars,
+    }
+
+    if S[N] == 'o'{
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+
+    
+}
