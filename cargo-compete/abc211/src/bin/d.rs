1a7c1374cceeb9f098ae5bff6bc922e47c61c836
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-08-28 23:39:28 +0900

    Submit abc211-d

--- cargo-compete/abc211/src/bin/d.rs
+++ cargo-compete/abc211/src/bin/d.rs
@@ -17,8 +17,68 @@ fn main() {
 
     input_edges!(N, M, edges, ad);
 
-    let mut m = 10i64.pow(9)+7;
-    
+    let mut d = vec![2*10i64.pow(5)+5;N];
+    let mut seen = vec![false;N];
+    let mut ord = vec![];
+    let start = 0;
+    d[start] = 0;
+    let mut todo = VecDeque::new();
+    todo.push_back(start);
+    loop {
+        if let Some(u) = todo.pop_front() {
+            if seen[u] {
+                continue;
+            }
+            seen[u] = true;
+            ord.push(u);
+            for &v in ad[u].iter(){
+                let dd = d[u] + 1;
+                if d[v].chmin(dd){
+                    todo.push_back(v);
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    let mut pat = vec![0;N];
+
+    let m = 10i64.pow(9) + 7;
+
+    pat[start] = 1;
+    for &u in ord.iter() {
+        for &v in ad[u].iter(){
+            if d[v] < d[u] {
+                pat[u] += pat[v];
+                pat[u] %= m;
+            }
+        }
+    }
+
+    println!("{}", pat[N-1]);
+}
+pub trait Change<T: PartialOrd> {
+    fn chmin(&mut self, rhs: Self) -> bool;
+    fn chmax(&mut self, rhs: Self) -> bool;
+}
+impl<T: PartialOrd> Change<T> for T {
+    fn chmax(&mut self, rhs: Self) -> bool {
+        if *self < rhs {
+            *self = rhs;
+            true
+        } else {
+            false
+        }
+    }
+    fn chmin(&mut self, rhs: Self) -> bool {
+        if *self > rhs {
+            *self = rhs;
+            true
+        } else {
+            false
+        }
+    }
 }
 //______________________________________________________________________________
 //
