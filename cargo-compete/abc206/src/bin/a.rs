122b3176bf0244e3829c5e13e74e9722e8a046d1
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-09-04 21:13:05 +0900

    Submit abc206-a,b,c

---
+++ cargo-compete/abc206/src/bin/a.rs
@@ -0,0 +1,69 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+use proconio::{input, fastout};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::fmt::{Write, Display};
+use std::collections::*;
+use maplit::*;
+use itertools::*;
+use superslice::{Ext, Ext2};
+
+#[fastout]
+fn main() {
+    input!{
+        N: f64,
+    }
+
+    let y = (N*1.08).floor() as i32;
+    let ans = match y.cmp(&206) {
+        std::cmp::Ordering::Less => "Yay!",
+        std::cmp::Ordering::Equal => "so-so",
+        std::cmp::Ordering::Greater => ":(",
+    };
+
+    println!("{}", ans);
+}
+//______________________________________________________________________________
+//
+pub trait Answer {
+    fn ans(&self);
+    fn ansl(&self);
+}
+
+impl<T: Display> Answer for Vec<T> {
+    fn ans(&self) {
+        let ans = self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join(" ");
+        println!("{}", ans);
+    }
+
+    fn ansl(&self) {
+        let ans = self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join("\n");
+        println!("{}", ans);
+    }
+}
+//______________________________________________________________________________
+//
+#[macro_export]
+macro_rules! input_edges {
+    ($n: expr, $m: expr, $edges: tt, $ad: tt) => {
+        input! {
+            $edges: [(Usize1, Usize1); $m],
+        }
+
+        let mut $ad = vec![vec![]; $n];
+        for &(a, b) in $edges.iter() {
+            $ad[a].push(b);
+            $ad[b].push(a);
+        }
+        let $ad = $ad;
+    };
+}
+
