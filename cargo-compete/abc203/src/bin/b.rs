929450600d4bd93a30956c1569b0f4b46e7a8277
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-09-07 23:02:39 +0900

    Submit abc203-a,b

---
+++ cargo-compete/abc203/src/bin/b.rs
@@ -0,0 +1,67 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+use itertools::*;
+use maplit::*;
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use proconio::{fastout, input};
+use std::collections::*;
+use std::fmt::{Display, Write};
+use superslice::{Ext, Ext2};
+
+#[fastout]
+fn main() {
+    input! {
+        N: i64,
+        K: i64,
+    }
+
+    let ans = (1..=N)
+        .cartesian_product((1..=K))
+        .map(|(i, j)| i * 100 + j)
+        .sum::<i64>();
+
+    println!("{}", ans);
+}
+//______________________________________________________________________________
+//
+pub trait Answer {
+    fn ans(&self);
+    fn ansl(&self);
+}
+
+impl<T: Display> Answer for Vec<T> {
+    fn ans(&self) {
+        let ans = self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join(" ");
+        println!("{}", ans);
+    }
+
+    fn ansl(&self) {
+        let ans = self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join("\n");
+        println!("{}", ans);
+    }
+}
+//______________________________________________________________________________
+//
+#[macro_export]
+macro_rules! input_edges {
+    ($n: expr, $m: expr, $edges: tt, $ad: tt) => {
+        input! {
+            $edges: [(Usize1, Usize1); $m],
+        }
+
+        let mut $ad = vec![vec![]; $n];
+        for &(a, b) in $edges.iter() {
+            $ad[a].push(b);
+            $ad[b].push(a);
+        }
+        let $ad = $ad;
+    };
+}
