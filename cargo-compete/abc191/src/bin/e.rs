0d012242ad5fa21474be0df56a575d4b86e72d92
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-09-24 12:44:53 +0900

    Submit abc191-e

--- cargo-compete/abc191/src/bin/e.rs
+++ cargo-compete/abc191/src/bin/e.rs
@@ -11,10 +11,97 @@ use superslice::{Ext, Ext2};
 #[fastout]
 fn main() {
     input!{
-        
+        N: usize, 
+        M: usize,
+        ABC: [(Usize1, Usize1, i64);M],
     }
 
+    let mut to = vec![vec![];N];
+    let mut from = vec![vec![];N];
+    for &(a, b, c) in ABC.iter(){
+        to[a].push((c, b));
+        from[b].push((c, a));
+    }
+
+    let mut ans = vec![0;N];
+    let inf = 2i64*10i64.pow(18);
+    for u in 0..N{
+        let mut d = vec![inf;N];
+        let mut todo = MinHeap::new();
+        todo.push((0,u));
+        loop{
+            if let Some((dd, v)) = todo.pop(){
+                if d[v].chmin(dd){
+                    for &(c, w) in to[v].iter(){
+                        todo.push((dd+c, w));
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        let min = from[u]
+            .iter()
+            .map(|&(c, v)|(c, d[v]))
+            .filter(|&(_, d)|d!=inf)
+            .map(|(c, d)|c + d)
+            .min()
+            .unwrap_or(-1);
+        ans[u] = min;
+    }
+
+    ans.ansl();
+
     
+}
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+impl<T: Ord> MinHeap<T> {
+    pub fn new() -> Self {
+        let heap = BinaryHeap::new();
+        MinHeap { heap }
+    }
+    pub fn push(&mut self, x: T) {
+        self.heap.push(Reverse(x));
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        self.heap.pop().map(|x| x.0)
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.heap.peek().map(|x| &x.0)
+    }
+    pub fn len(&self) -> usize {
+        self.heap.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.heap.is_empty()
+    }
+}
+#[derive(Clone, Debug)]
+struct MinHeap<T> {
+    heap: BinaryHeap<Reverse<T>>,
+}
+pub trait Change<T: PartialOrd> {
+    fn chmin(&mut self, rhs: Self) -> bool;
+    fn chmax(&mut self, rhs: Self) -> bool;
+}
+impl<T: PartialOrd> Change<T> for T {
+    fn chmax(&mut self, rhs: Self) -> bool {
+        if *self < rhs {
+            *self = rhs;
+            true
+        } else {
+            false
+        }
+    }
+    fn chmin(&mut self, rhs: Self) -> bool {
+        if *self > rhs {
+            *self = rhs;
+            true
+        } else {
+            false
+        }
+    }
 }
 //______________________________________________________________________________
 //
