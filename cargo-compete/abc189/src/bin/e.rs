768aaf42c5c1a05fc1bc392822f87ba53d6cee7c
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-09-27 22:56:48 +0900

    Submit abc189-e

--- cargo-compete/abc189/src/bin/e.rs
+++ cargo-compete/abc189/src/bin/e.rs
@@ -7,13 +7,81 @@ use std::collections::*;
 use maplit::*;
 use itertools::*;
 use superslice::{Ext, Ext2};
+use nalgebra::{Matrix3, Vector3};
 
 #[fastout]
 fn main() {
     input!{
-        
+        N: usize,
+        XY: [(i64, i64);N],
+        M: usize,
+    }
+    let mut mat = Matrix3::identity();
+    let mut mats = Vec::with_capacity(M+1);
+    mats.push(mat.clone());
+    let r = Matrix3::new(
+        0, 1, 0,
+        -1, 0, 0,
+        0, 0, 1
+    );
+    let s = Matrix3::new(
+        0, -1, 0,
+        1, 0, 0,
+        0, 0, 1
+    );
+
+    for _ in 0..M{
+        input!{
+            t: i32,
+        }
+        match t {
+            1 => {
+                mat = r * mat;
+                mats.push(mat.clone());
+            }
+            2 => {
+                mat = s * mat;
+                mats.push(mat.clone());
+            }
+            3 => {
+                input!{
+                    p: i64,
+                }
+                let a = Matrix3::new(
+                    -1, 0, 2 * p,
+                    0, 1, 0,
+                    0, 0, 1
+                );
+                mat = a * mat;
+                mats.push(mat.clone());
+            },
+            4 => {
+                input!{
+                    p: i64,
+                }
+                let a = Matrix3::new(
+                    1, 0, 0,
+                    0, -1, 2*p,
+                    0, 0, 1
+                );
+                mat = a * mat;
+                mats.push(mat.clone());
+            },
+            _ => unreachable!(),
+        }
     }
 
+    input!{
+        Q: usize,
+        AB: [(usize, Usize1);Q],
+    }
+
+    for &(a, b) in AB.iter(){
+        let (x, y) = XY[b];
+        let v = Vector3::new(x, y, 1);
+        let w = mats[a] * v;
+        println!("{} {}", w[0], w[1]);
+    }
     
 }
 //______________________________________________________________________________
