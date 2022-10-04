8cd6b430101bd5e6c63a581a6fb4fabc028f0b41
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-10-04 11:22:44 +0900

    Submit abc186-a,b,c

---
+++ cargo-compete/abc186/src/bin/b.rs
@@ -0,0 +1,76 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+use proconio::{input, fastout};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::fmt::{Write, Display};
+use std::collections::*;
+use maplit::*;
+use itertools::*;
+use superslice::{Ext, Ext2};
+
+#[fastout]
+fn main() {
+    input!{
+        H: usize,
+        W: usize,
+        A: [[i64;W];H],
+    }
+
+    let min = A.iter().map(|v|v.iter().min().unwrap()).min().unwrap();
+    let ans = A.iter().map(|v|v.iter().map(|x|x-min).sum::<i64>()).sum::<i64>();
+    println!("{}", ans);
+
+    
+}
+//______________________________________________________________________________
+//
+pub trait Answer {
+    fn fmt(&self)->String;
+    fn fmtl(&self)->String;
+    fn ans(&self);
+    fn ansl(&self);
+}
+
+impl<T: Display> Answer for Vec<T> {
+    fn fmt(&self)->String {
+        self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join(" ")
+    }
+
+    fn ans(&self) {
+        println!("{}", self.fmt());
+    }
+
+    fn fmtl(&self)->String {
+        self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn ansl(&self) {
+        println!("{}", self.fmtl());
+    }
+}
+//______________________________________________________________________________
+//
+#[macro_export]
+macro_rules! input_edges {
+    ($n: expr, $m: expr, $edges: tt, $ad: tt) => {
+        input! {
+            $edges: [(Usize1, Usize1); $m],
+        }
+
+        let mut $ad = vec![vec![]; $n];
+        for &(a, b) in $edges.iter() {
+            $ad[a].push(b);
+            $ad[b].push(a);
+        }
+        let $ad = $ad;
+    };
+}
+
