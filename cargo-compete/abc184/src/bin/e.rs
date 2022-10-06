62a943e61d210ff09f816377aedf622711936ca7
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-10-07 02:58:39 +0900

    Submit abc184-e

--- cargo-compete/abc184/src/bin/e.rs
+++ cargo-compete/abc184/src/bin/e.rs
@@ -1,34 +1,92 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
-use proconio::{input, fastout};
-use proconio::marker::{Bytes, Chars, Isize1, Usize1};
-use std::fmt::{Write, Display};
-use std::collections::*;
-use maplit::*;
 use itertools::*;
+use maplit::*;
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use proconio::{fastout, input};
+use std::collections::*;
+use std::fmt::{Display, Write};
 use superslice::{Ext, Ext2};
+
+fn atoi(a: char) -> usize {
+    (a as u8 - b'a') as usize
+}
+
+fn isa(a: char) -> bool {
+    'a' <= a && a <= 'z'
+}
 
 #[fastout]
 fn main() {
-    input!{
-        
+    input! {
+        H: usize,
+        W: usize,
+        a: [Chars;H],
     }
 
-    
+    let (sx, sy) = (0..H)
+        .cartesian_product(0..W)
+        .find(|&(i, j)| a[i][j] == 'S')
+        .unwrap();
+    let (gx, gy) = (0..H)
+        .cartesian_product(0..W)
+        .find(|&(i, j)| a[i][j] == 'G')
+        .unwrap();
+    let mut t = vec![vec![]; 26];
+    for i in 0..H {
+        for j in 0..W {
+            if isa(a[i][j]) {
+                t[atoi(a[i][j])].push((i, j));
+            }
+        }
+    }
+
+    let mut todo = VecDeque::new();
+    todo.push_back((sx, sy));
+    let mut dd = vec![vec![None; W]; H];
+    dd[sx][sy] = Some(0);
+    let mut used = vec![false; 26];
+    loop {
+        if let Some((x, y)) = todo.pop_front() {
+            if isa(a[x][y]) {
+                let i = atoi(a[x][y]);
+                if !used[i] {
+                    used[i] = true;
+                    for &(a, b) in t[i].iter() {
+                        if dd[a][b] == None {
+                            todo.push_back((a, b));
+                            dd[a][b] = Some(dd[x][y].unwrap() + 1);
+                        }
+                    }
+                }
+            }
+            for (dx, dy) in &[(1, 0), (!0, 0), (0, 1), (0, !0)] {
+                if x + dx < H && y + dy < W && a[x + dx][y + dy] != '#' {
+                    if dd[x + dx][y + dy] == None {
+                        dd[x + dx][y + dy] = Some(dd[x][y].unwrap() + 1);
+                        todo.push_back((x + dx, y + dy));
+                    }
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", dd[gx][gy].unwrap_or(-1));
 }
 //______________________________________________________________________________
 //
 pub trait Answer {
-    fn fmt(&self)->String;
-    fn fmtl(&self)->String;
+    fn fmt(&self) -> String;
+    fn fmtl(&self) -> String;
     fn ans(&self);
     fn ansl(&self);
 }
 
 impl<T: Display> Answer for Vec<T> {
-    fn fmt(&self)->String {
-        self
-            .iter()
+    fn fmt(&self) -> String {
+        self.iter()
             .map(|x| format!("{}", x))
             .collect::<Vec<_>>()
             .join(" ")
@@ -38,9 +96,8 @@ impl<T: Display> Answer for Vec<T> {
         println!("{}", self.fmt());
     }
 
-    fn fmtl(&self)->String {
-        self
-            .iter()
+    fn fmtl(&self) -> String {
+        self.iter()
             .map(|x| format!("{}", x))
             .collect::<Vec<_>>()
             .join("\n")
@@ -67,4 +124,3 @@ macro_rules! input_edges {
         let $ad = $ad;
     };
 }
-
