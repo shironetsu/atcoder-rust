edbb19b7b1a2015e0823d03a4bdd4cdd23485098
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-10-07 03:07:33 +0900

    Submit abc184-f

--- cargo-compete/abc184/src/bin/f.rs
+++ cargo-compete/abc184/src/bin/f.rs
@@ -11,10 +11,62 @@ use superslice::{Ext, Ext2};
 #[fastout]
 fn main() {
     input!{
-        
+        N: usize,
+        T: i64,
+        A: [i64;N],
     }
 
+    let a = &A[0..N/2];
+    let b = &A[N/2..N];
+
+    let mut p = Vec::with_capacity(1<<a.len());
+    let mut q = Vec::with_capacity(1<<b.len());
+
+    for s in 0..1<<a.len(){
+        let sum = (0..a.len()).filter(|&i|(s>>i)&1==1).map(|i|a[i]).sum::<i64>();
+        p.push(sum);
+    }
+    for t in 0..1<<b.len(){
+        let sum = (0..b.len()).filter(|&i|(t>>i)&1==1).map(|i|b[i]).sum::<i64>();
+        q.push(sum);
+    }
+
+    p.sort();
+    q.sort();
+
+    let mut ans = 0;
+    for &x in p.iter(){
+        let y = T - x;
+        let i = q.upper_bound(&y);
+        if 0 < i {
+            ans.chmax(x + q[i-1]);
+        }
+    }
+
+    println!("{}", ans);
     
+}
+pub trait Change<T: PartialOrd> {
+    fn chmin(&mut self, rhs: Self) -> bool;
+    fn chmax(&mut self, rhs: Self) -> bool;
+}
+impl<T: PartialOrd> Change<T> for T {
+    fn chmax(&mut self, rhs: Self) -> bool {
+        if *self < rhs {
+            *self = rhs;
+            true
+        } else {
+            false
+        }
+    }
+    fn chmin(&mut self, rhs: Self) -> bool {
+        if *self > rhs {
+            *self = rhs;
+            true
+        } else {
+            false
+        }
+    }
 }
 //______________________________________________________________________________
 //
