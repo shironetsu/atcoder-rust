bc0c1709916e30f37ade15753b7ce1f0be08b344
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-10-19 23:04:29 +0900

    Submit abc273-d

--- cargo-compete/abc273/src/bin/d.rs
+++ cargo-compete/abc273/src/bin/d.rs
@@ -11,23 +11,77 @@ use superslice::{Ext, Ext2};
 #[fastout]
 fn main() {
     input!{
-        H: usize,
-        W: usize,
-        rs: Usize1,
-        cs: Usize1,
+        H: isize,
+        W: isize,
+        rs: Isize1,
+        cs: Isize1,
         N: usize,
-        rc: [(Usize1,Usize1);N],
+        rc: [(Isize1,Isize1);N],
         Q: usize,
-        dl: [(char, usize);Q],
+        dl: [(char, isize);Q],
     }
 
-    let mut a = BTreeSet::new();
-    let mut b = BTreeSet::new();
+    let mut a = BTreeMap::new();
+    let mut b = BTreeMap::new();
 
     for &(r, c) in rc.iter(){
-        *a.entry(r).or_insert(BTreeSet::new()).insert(c);
-        *b.entry(c).or_insert(BTreeSet::new()).insert(r);
+        a.entry(r).or_insert(btreeset![-1,W]).insert(c);
+        b.entry(c).or_insert(btreeset![-1,H]).insert(r);
     }
+
+    let mut x = rs;
+    let mut y = cs;
+    for &(d, l) in dl.iter(){
+        match d {
+            'L' => {
+                if let Some(s) = a.get_mut(&x) {
+                    if let Some(&yy) = s.range(y-l..y).next_back() {
+                        y = yy + 1;
+                    } else {
+                        y = y - l;
+                    }
+                } else {
+                    y = (y-l).max(0);
+                }
+            },
+            'R' => {
+                if let Some(s) = a.get_mut(&x) {
+                    if let Some(&yy) = s.range(y+1..=y+l).next() {
+                        y = yy - 1;
+                    } else {
+                        y = y + l;
+                    }
+                } else {
+                    y = (y+l).min(W-1);
+                }
+            },
+            'U' => {
+                if let Some(s) = b.get_mut(&y) {
+                    if let Some(&xx) = s.range(x-l..x).next_back() {
+                        x = xx + 1;
+                    } else {
+                        x = x - l;
+                    }
+                } else {
+                    x = (x-l).max(0);
+                }
+            },
+            'D' => {
+                if let Some(s) = b.get_mut(&y) {
+                    if let Some(&xx) = s.range(x+1..=x+l).next() {
+                        x = xx - 1;
+                    } else {
+                        x = x + l;
+                    }
+                } else {
+                    x = (x+l).min(H-1);
+                }
+            },
+            _ => unreachable!(),
+        }
+        println!("{} {}", x+1, y+1);
+    }
+
 
     
 
