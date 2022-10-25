4655ab8f80af11bf6cf4e8f85f2c87f49f48ee5b
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-10-25 21:36:47 +0900

    Submit abc172-a,b

---
+++ cargo-compete/abc172/src/bin/b.rs
@@ -0,0 +1,83 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+use itertools::*;
+use maplit::*;
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use proconio::{fastout, input};
+use std::collections::*;
+use std::fmt::{Display, Write};
+use superslice::{Ext, Ext2};
+
+#[fastout]
+fn main() {
+    input! {
+        S: Chars ,
+        T: Chars,
+    }
+
+    let ans = S
+        .iter()
+        .zip(T.iter())
+        .fold(0, |acc, (&s, &t)| acc + if s != t { 1 } else { 0 });
+    println!("{}", ans);
+}
+//______________________________________________________________________________
+//
+pub trait ToString {
+    fn to_string(&self) -> String;
+}
+
+impl ToString for [char] {
+    fn to_string(&self) -> String {
+        self.iter().collect::<String>()
+    }
+}
+//______________________________________________________________________________
+//
+pub trait Answer {
+    fn fmt(&self) -> String;
+    fn fmtl(&self) -> String;
+    fn ans(&self);
+    fn ansl(&self);
+}
+
+impl<T: Display> Answer for Vec<T> {
+    fn fmt(&self) -> String {
+        self.iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join(" ")
+    }
+
+    fn ans(&self) {
+        println!("{}", self.fmt());
+    }
+
+    fn fmtl(&self) -> String {
+        self.iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn ansl(&self) {
+        println!("{}", self.fmtl());
+    }
+}
+//______________________________________________________________________________
+//
+#[macro_export]
+macro_rules! input_edges {
+    ($n: expr, $m: expr, $edges: tt, $ad: tt) => {
+        input! {
+            $edges: [(Usize1, Usize1); $m],
+        }
+
+        let mut $ad = vec![vec![]; $n];
+        for &(a, b) in $edges.iter() {
+            $ad[a].push(b);
+            $ad[b].push(a);
+        }
+        let $ad = $ad;
+    };
+}
