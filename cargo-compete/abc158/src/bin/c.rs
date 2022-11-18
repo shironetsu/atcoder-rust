b93cbe7602bc18943a80251b9bc6ab8c5b6ea150
shironetsu <70092342+shironetsu@users.noreply.github.com>
2022-11-18 23:55:57 +0900

    Submit abc158-a,b

---
+++ cargo-compete/abc158/src/bin/c.rs
@@ -0,0 +1,102 @@
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+use proconio::{input, fastout};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::fmt::{Write, Display};
+use std::collections::*;
+use maplit::*;
+use itertools::*;
+use superslice::{Ext, Ext2};
+
+#[fastout]
+fn main() {
+    input!{
+        A: usize,
+        B: usize,
+    }
+
+    let mut inf = 1_000;
+    let mut t = vec![vec![inf;B+1];A+1];
+    for y in (0..=200).rev(){
+        let a = y * 108 / 100 - y;
+        let b = y * 110 / 100 - y;
+        if a <= A && b <= B {
+            t[a][b] = y;
+        }
+    }
+
+    if t[A][B] < inf {
+        let mut y = t[A][B];
+        if y == 0 {
+            y += 100;
+        }
+        println!("{}", y);
+    } else {
+        println!("{}", -1);
+    }
+
+    
+}
+//______________________________________________________________________________
+//
+pub trait ToString {
+    fn to_string(&self) -> String;
+}
+
+impl ToString for [char] {
+    fn to_string(&self) -> String {
+        self.iter().collect::<String>()
+    }
+}
+//______________________________________________________________________________
+//
+pub trait Answer {
+    fn fmt(&self)->String;
+    fn fmtl(&self)->String;
+    fn ans(&self);
+    fn ansl(&self);
+}
+
+impl<T: Display> Answer for Vec<T> {
+    fn fmt(&self)->String {
+        self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join(" ")
+    }
+
+    fn ans(&self) {
+        println!("{}", self.fmt());
+    }
+
+    fn fmtl(&self)->String {
+        self
+            .iter()
+            .map(|x| format!("{}", x))
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn ansl(&self) {
+        println!("{}", self.fmtl());
+    }
+}
+//______________________________________________________________________________
+//
+#[macro_export]
+macro_rules! input_edges {
+    ($n: expr, $m: expr, $edges: tt, $ad: tt) => {
+        input! {
+            $edges: [(Usize1, Usize1); $m],
+        }
+
+        let mut $ad = vec![vec![]; $n];
+        for &(a, b) in $edges.iter() {
+            $ad[a].push(b);
+            $ad[b].push(a);
+        }
+        let $ad = $ad;
+    };
+}
+
